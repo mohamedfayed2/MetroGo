280d5d7acf7bab21c544d243b5a7dea0ea73f9d5
hfkghgk <mohamedadelmobark2020@gmail.com>
2025-03-19 17:30:21 -0700

    Regenerated pubspec.lock

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:metro_app/App_Screens/HomePage.dart';
+import 'App_Screens/MapPage.dart';
 import 'controllers/NavigationController.dart';
-import 'App_Screens/onboarding_screen.dart';
 import 'controllers/bindings.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Get.put(NavigationController());
   await GetStorage.init();
+  Get.put(NavigationController());
   runApp(MyApp());
 }
 
@@ -19,8 +19,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-        initialBinding: AppBindings(),
-        debugShowCheckedModeBanner: false,
-        home: HomePage());
+      initialBinding: AppBindings(),
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      getPages: [
+        GetPage(name: '/', page: () => HomePage()),
+        GetPage(name: '/map', page: () => MapPage()),
+      ],
+    );
   }
 }
