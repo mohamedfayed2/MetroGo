2b0f002249fc77b195c5f3babb8e8ba4d624a37c
kareem250 <ahmadkireem6@gmail.com>
2025-03-24 03:23:40 +0200

    fix in Android-Diractory

--- lib/App_Screens/MapPage.dart
+++ lib/App_Screens/MapPage.dart
@@ -1,16 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong2/latlong.dart';
-import 'package:flutter_typeahead/flutter_typeahead.dart';
-import 'package:dio/dio.dart' as dio;
 import '../Customs/Custom_Bottom_Navigation_Bar.dart';
 import '../controllers/NavigationController.dart';
-import 'package:flutter_map_cancellable_tile_provider/flutter_map_cancellable_tile_provider.dart';
-
-import 'PhotomapPage.dart';
-
-
 
 class MapPage extends StatelessWidget {
   final NavigationController navController = Get.find<NavigationController>();
@@ -25,15 +17,14 @@ class MapPage extends StatelessWidget {
         title: const Text("خريطة التنقل"),
         backgroundColor: Color(0xff007BFF),
       ),
-      body:Center(
+      body: Center(
         child: Container(
           width: double.infinity,
           child: Image.asset("assets/images/new-map8.jpg"),
         ),
       ),
-      bottomNavigationBar: SizedBox(
-          height: 80,
-          child: CustomBottomNavigationBar()),
+      bottomNavigationBar:
+          SizedBox(height: 80, child: CustomBottomNavigationBar()),
     );
   }
 }
